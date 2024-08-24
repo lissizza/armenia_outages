99ac8852b966b0636e258fa8259f23dc05ec4136
Irina Silvestrova <lissizza@gmail.com>
2024-08-24 21:19:03 +0400

    fix chromedriver install and path

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -6,16 +6,19 @@ echo "Updating translations..."
 
 # Install Chrome for Selenium
 echo "Installing Chrome and ChromeDriver..."
-CHROME_VERSION="google-chrome-stable"
-wget https://dl.google.com/linux/direct/$CHROME_VERSION_current_amd64.deb
+
+# Install dependencies
+apt-get update
+apt-get install -y wget unzip
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
+# Install ChromeDriver
 CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/chromedriver
-chmod +x ~/chromedriver/chromedriver
-export PATH=$PATH:~/chromedriver
+wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -P /app
+unzip /app/chromedriver_linux64.zip -d /app/chromedriver
+chmod +x /app/chromedriver/chromedriver
+export PATH=$PATH:/app/chromedriver
 
-# Start the bot
-echo "Starting the bot..."
-python bot.py
